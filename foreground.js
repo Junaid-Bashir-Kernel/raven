34b6a1f82cbf8b6cb003723253a68446b4e0eca6
junaid bashir <89861553+Junaid-Bashir-Kernel@users.noreply.github.com>
2022-10-06 23:58:26 +0530

    done1

--- foreground.js
+++ foreground.js
@@ -1,2 +1,15 @@
-//alert("hello welcome to github");
-document.body.style.backgroundColor="tomato;"
+setTimeout(()=>{
+
+ let p=document.querySelectorAll("h1")[0]
+ if(p != undefined){
+  p.style.color="tomato";
+  p.textContent="2022";
+ }
+
+
+},2000)
+
+
+
+
+
